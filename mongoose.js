2618b0b25177e37a1776b6532c13f709c8a16ce6
Shivam Prajapati <shivamprajapati.git@gmail.com>
2019-12-31 12:34:05 +0530

    validation using mongooose

--- mongoose.js
+++ mongoose.js
@@ -2,8 +2,25 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{newUrlParser:true});
 
-const User = mongoose.model('user',{name:String,age:Number});
-const shivam = new User({name:"shivam",age:20});
+const User = mongoose.model('user',{
+  name:{
+    type:String
+  },password:{
+    type:String,
+    trim:true,
+    validate(value){
+      if(value.length<=6||value==="password"){
+        throw new Error("Invalid password")
+      }
+    }
+  },
+  age:{
+    type:Number
+  }
+});
+
+
+const shivam = new User({name:"shivam",password:"shivam",age:20});
 shivam.save().then(()=>{
   console.log('saved');
 }).catch((err)=>{
