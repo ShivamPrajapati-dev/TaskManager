9659dd7019e7a861bf7b9512aee9b38dfa9e9583
Shivam Prajapati <shivamprajapati.git@gmail.com>
2020-01-06 18:01:19 +0530

    added reading data from db

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const User = require('./models/task');
+const User = require('./models/user');
+const Task = require('./models/task');
 const port = process.env.PORT||5000;
 app.use(express.json());
 
-app.get('/users',(req,res)=>{
+app.post('/user',(req,res)=>{
 
   const user = new User(req.body);
   user.save().then(()=>{
@@ -14,6 +15,29 @@ app.get('/users',(req,res)=>{
   })
 
 });
+
+
+app.get('/user',(req,res)=>{
+
+  User.find({}).then((user)=>{
+    res.status(201).send(user);
+  }).catch((err)=>{
+    res.status(500).send(err);
+  });
+
+});
+
+app.post('/task',(req,res)=>{
+
+  const task = new Task(req.body);
+  task.save().then(()=>{
+    res.status(201).send(task)
+  }).catch((err)=>{
+    res.status(400).send(err);
+  })
+
+
+})
 
 app.listen(port,()=>{
   console.log("listening on port "+port);
