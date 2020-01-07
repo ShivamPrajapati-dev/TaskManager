d6d883dc6ab81816e537aaf64f75d2d9a63e3657
Shivam Prajapati <shivamprajapati.git@gmail.com>
2020-01-07 17:56:04 +0530

    refractor

---
+++ router/task.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = new express.Router();
+const Task = require('../models/task');
+
+router.get('/task',async (req,res)=>{
+
+try {
+  const tasks = await Task.find({});
+  res.send(tasks);
+} catch (e) {
+  res.status(500).send()
+}
+
+});
+
+router.get('/task/:id',async (req,res)=>{
+
+  const _id = req.params.id;
+  try{
+    const task = await Task.findById(_id);
+    if(!task){
+      return res.status(404).send();
+    }
+    res.send(task);
+  }catch(e){
+    res.status(500).send()
+  }
+
+});
+
+
+
+router.delete('/task/:id',async (req,res)=>{
+  try{
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task){
+      res.status(404).send();
+    }
+    res.send(task);
+  }catch(e){
+    res.status.send();
+  }
+});
+
+
+router.patch('/task/:id', async (req,res)=>{
+  const validUpdates = ['name','description'];
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every((update)=>{
+    return validUpdates.includes(update);
+  });
+  if(!isValidOperation){
+    return res.status(400).send();
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+    if(!task){
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post('/task',async (req,res)=>{
+
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+
+});
+
+
+module.exports = router;
